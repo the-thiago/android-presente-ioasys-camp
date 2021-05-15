3a5a5a6e1f0802d4b92d336eca2fb23133eba01d
Thiago Moreira de Souza <thiago.moreira.souza@gmail.com>
2021-05-15 12:51:26 -0300

    Splash screen

---
+++ app/src/main/java/com/br/equipe/oito/presente/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.br.equipe.oito.presente
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+}
---
+++ app/src/main/java/com/br/equipe/oito/presente/SplashActivity.kt
@@ -0,0 +1,21 @@
+package com.br.equipe.oito.presente
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this@SplashActivity, LoginActivity::class.java))
+            finish()
+        }, 1500)
+    }
+
+}
