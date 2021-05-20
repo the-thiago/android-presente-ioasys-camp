2af6d56ab9c25c46d854c286faff0706b04f2022
Thiago Moreira de Souza <thiago.moreira.souza@gmail.com>
2021-05-20 19:41:10 -0300

    First login fragment layout

---
+++ app/src/main/java/com/br/equipe/oito/presente/FirstLoginFragment.kt
@@ -0,0 +1,18 @@
+package com.br.equipe.oito.presente
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class FirstLoginFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_first_login, container, false)
+    }
+
+}
