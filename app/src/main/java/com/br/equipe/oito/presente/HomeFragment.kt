a6f0cee84061953fe791eb06f36dcd006fb4df36
Thiago Moreira de Souza <thiago.moreira.souza@gmail.com>
2021-05-25 20:16:19 -0300

    Home toolbar

--- app/src/main/java/com/br/equipe/oito/presente/HomeFragment.kt
+++ app/src/main/java/com/br/equipe/oito/presente/HomeFragment.kt
@@ -1,18 +1,37 @@
 package com.br.equipe.oito.presente
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.br.equipe.oito.presente.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initListener()
+    }
+
+    private fun initListener() {
+
     }
 
 }
